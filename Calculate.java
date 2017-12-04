a3a463a3f71311f12c6315e70321e97370505ff1
Akki357358 <akashkothawade910@gmail.com>
2017-12-04 16:14:22 +0530

    first commit

---
+++ Calculate.java
@@ -0,0 +1,25 @@
+public class Calculate {
+
+	public double add(double x, double y) {
+		return x + y;
+	}
+	
+	public double sub(double x, double y) {
+		return x - y;
+	}
+	
+	public double mult(double x, double y) {
+		return x * y;
+	}
+	
+	public double div(double x, double y) {
+		return x / y;
+	}
+	public double sqr(double x){
+		return x * x;
+	}
+	public double percent(double x, double y)
+	{
+		return ((x / y) * 100);
+	}
+}
